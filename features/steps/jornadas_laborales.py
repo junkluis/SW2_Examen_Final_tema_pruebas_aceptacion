589210c86ab8642de890e8310057fb0125909522
dfsaigua <dfsaigua@espol.edu.ec>
2019-08-26 10:06:51 -0500

    aceptacion1

--- features/steps/jornadas_laborales.py
+++ features/steps/jornadas_laborales.py
@@ -23,3 +23,23 @@ def step_impl(context, variable):
 @then("tambien ocurre lo siguiente (si es necesario)")
 def step_impl(context):
 	pass
+
+@given("horario diurno o nocturno")
+def step_impl(context):
+	context.horario = empleado[horario]
+	if context.horarios=="Diurno A" || context.horarios=="Diurno B" context.horarios=="Nocturno A" || context.horario=="Nocturno B"
+			mensaje="exito"
+
+@when("el usuario ingresa la hora: '{horario}'")
+def step_impl(context, empleado[horario]):
+	context.horario = empleado[horario]
+
+@when("el usuario ingresa la hora: '{codigo}'")
+def step_impl(context, empleado[cod_trabajador]):
+	context.codigo = empleado[cod_trabajador]
+
+
+@then("genera el siguiete resultado '{variable}'")
+def step_impl(context, variable):
+	
+	
