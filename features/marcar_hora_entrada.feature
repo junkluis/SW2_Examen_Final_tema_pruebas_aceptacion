783851b802e9d55e4740a87c8433deb69f53c617
Estudiante <xav_andrade@hotmail.com>
2019-08-26 05:14:52 -0500

    Dos features

--- features/marcar_hora_entrada.feature
+++ features/marcar_hora_entrada.feature
@@ -2,9 +2,17 @@
 
 Característica: Registrar hora de entrada
 
- @jornadasDeIngreso
+  @jornadasDeIngreso
   Escenario: 
-      Dado que se cumplen los requisitos
-      Cuando se ejecute una accion
-      Entonces genera el siguiete resultado 'resultado_variable'
-      Y tambien ocurre lo siguiente (si es necesario)
+      Dado que mi horario es Diurno B
+      Cuando ingrese mi entrada con retraso de 00:14:59 a mi hora de marcado 13:00 el dia martes
+      Y ingrese mi codigo de trabajador: 20153
+      Entonces se buscara mi nombre
+      Y aparecera el mensaje: Inicio de Jornada a tiempo, Bienvenido Jon Snow.
+
+  @jornadasDeIngreso
+  Escenario: 
+      Dado que mi horario es Diurno B
+      Cuando ingrese entrada en un dia (miercoles) con hora 7:00 que no este mi horario
+      Y ingrese mi codigo de trabajador: 20153
+      Entonces aparecera el mensaje: EL día miercoles no tiene jornada laboral programada.
--- features/steps/jornadas_laborales.py
+++ features/steps/jornadas_laborales.py
@@ -7,19 +7,53 @@ def before_scenario(context, scenario):
 	context = {}
 
 
-@given("que se cumplen los requisitos")
-def step_impl(context):
-	pass
+@given("que mi horario es {tipo_horario}")
+def step_impl(context, tipo_horario):
+    es_valido = tipo_horario == "Nocturno A" or tipo_horario == "Nocturno B"\
+            or tipo_horario == "Diurno A" or tipo_horario == "Diurno B"
+    assert es_valido == True
+    context.tipo_horario = tipo_horario
 
-@when("se ejecute una accion")
-def step_impl(context):
-	pass
 
-@then("genera el siguiete resultado '{variable}'")
-def step_impl(context, variable):
-	print(variable)
-	pass
+@when("ingrese mi entrada con retraso de {tiempo} a mi hora de marcado {hora} el dia {dia}")
+def step_impl(context, tiempo, hora, dia):
+    tiempo = tiempo.split(":")
+    context.horas = int(tiempo[0])
+    context.minutos = int(tiempo[1])
+    context.segundos = int(tiempo[2])
 
-@then("tambien ocurre lo siguiente (si es necesario)")
+    hora = hora.split(":")
+    context.hora = int(hora[0])
+    context.minuto = int(hora[1])
+    context.dia = dia
+    
+
+@when("ingrese mi codigo de trabajador: {codigo}")
+def step_impl(context, codigo):
+    context.codigo = codigo
+
+
+@then("se buscara mi nombre")
 def step_impl(context):
-	pass
+    context.empleado = empleados[context.codigo][0]
+    horario = empleados[context.codigo][1]
+    assert context.tipo_horario == horario
+        
+
+@then("aparecera el mensaje: {mensaje}")
+def step_impl(context, mensaje):
+    msj, empleado = marcar_hora_entrada(context.codigo, context.dia,
+                                    context.hora, context.minutos,
+                                    context.segundos)
+    assert msj == mensaje
+
+
+@when("ingrese entrada en un dia ({dia}) con hora {tiempo} que no este mi horario")
+def step_impl(context, dia, tiempo):
+    context.dia = dia
+    tiempo = tiempo.split(":")
+    context.hora = int(tiempo[0])
+    context.minuto = int(tiempo[1])
+    context.horas = 0
+    context.minutos = 0
+    context.segundos = 0
