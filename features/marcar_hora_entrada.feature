68043a58303a4cfb8bb6fff9ec67d4aef963b7a5
leandre96 <leo_silver12@hotmail.com>
2019-08-26 10:16:16 -0500

    updating files

--- features/marcar_hora_entrada.feature
+++ features/marcar_hora_entrada.feature
@@ -2,9 +2,34 @@
 
 Característica: Registrar hora de entrada
 
- @jornadasDeIngreso
+ @jornadasDeIngreso1
   Escenario: 
-      Dado que se cumplen los requisitos
-      Cuando se ejecute una accion
-      Entonces genera el siguiete resultado 'resultado_variable'
-      Y tambien ocurre lo siguiente (si es necesario)
+      Dado que mi horario de trabajo puede ser diurno o nocturno
+	  Cuando se ingrese mi hora de entrada '7' hora '14' minutos '59' el dia 'lunes' hasta con un retraso de 14 minutos con 59 segundos a mi hora de marcado e ingrese mi codigo de trabajador '20111'
+      Entonces se buscara mi nombre en la lista
+      Y aparecera el siguiente mensaje 'Inicio de Jornada a tiempo, Bienvenido Tyrion Lannister.'
+	  Y devolvera el tipo de jornada laboral 'Diurno A'
+	  
+ @jornadasDeIngreso2
+  Escenario: 
+      Dado que mi horario de trabajo puede ser diurno o nocturno
+	  Cuando se ingrese mi hora de entrada '1' hora '12' minutos '59' segundos un dia que no este 'martes' en mi horario e ingrese mi codigo de trabajador '20111'
+      Entonces se buscara mi nombre en la lista
+      Y aparecera el siguiente mensaje 'EL día martes no tiene jornada laboral programada.'
+	  Y devolvera el tipo de jornada laboral 'Diurno A'
+	  
+ @jornadasDeIngreso3
+  Escenario: 
+      Dado que mi horario de trabajo puede ser diurno o nocturno
+	  Cuando se ingrese mi hora de entrada '7' hora '16' minutos '15' el dia 'lunes' con un retraso mayor o igual a 15 minutos, pero menor a 1 hora, e ingrese mi codigo de trabajador '20111'
+      Entonces se buscara mi nombre en la lista
+      Y aparecera el siguiente mensaje 'Inicio de Jornada atrasada por 0:01:16.'
+	  Y devolvera el tipo de jornada laboral 'Diurno A'
+	  
+ @jornadasDeIngreso4
+  Escenario: 
+      Dado que mi horario de trabajo puede ser diurno o nocturno
+	  Cuando se ingrese mi hora de entrada '6' hora '39' minutos '0' el dia 'lunes' antes de lo esperado (20 minutos antes de mi hora de entrada) e ingrese mi codigo de trabajador '20111'
+      Entonces se buscara mi nombre en la lista
+      Y aparecera el siguiente mensaje 'Su turno empieza en 0:01:01.'
+	  Y devolvera el tipo de jornada laboral 'Diurno A'
--- features/steps/jornadas_laborales.py
+++ features/steps/jornadas_laborales.py
@@ -7,19 +7,45 @@ def before_scenario(context, scenario):
 	context = {}
 
 
-@given("que se cumplen los requisitos")
+@given("que mi horario de trabajo puede ser diurno o nocturno")
 def step_impl(context):
 	pass
 
-@when("se ejecute una accion")
-def step_impl(context):
-	pass
+@when("se ingrese mi hora de entrada '{hora}' hora '{minuto}' minutos '{segundo}' el dia '{dia}' hasta con un retraso de 14 minutos con 59 segundos a mi hora de marcado e ingrese mi codigo de trabajador '{codigo}'")
+def step_impl(context,hora,minuto,segundo,dia,codigo):
+	mensaje,turno = marcar_hora_entrada(codigo, dia, int(hora), int(minuto), int(segundo))
+	context.mensaje=mensaje
+	context.turno=turno
+	
 
-@then("genera el siguiete resultado '{variable}'")
-def step_impl(context, variable):
-	print(variable)
-	pass
-
-@then("tambien ocurre lo siguiente (si es necesario)")
+@then("se buscara mi nombre en la lista")
 def step_impl(context):
-	pass
+        context.nombre='Tyrion Lannister'
+
+@then("aparecera el siguiente mensaje '{mensaje}'")
+def step_impl(context,mensaje):
+        print(context.mensaje)
+        assert context.mensaje == mensaje
+	
+@then("devolvera el tipo de jornada laboral '{turno}'")
+def step_impl(context,turno):
+        print(context.turno)
+        assert context.turno == turno
+
+@when("se ingrese mi hora de entrada '{hora}' hora '{minuto}' minutos '{segundo}' segundos un dia que no este '{dia}' en mi horario e ingrese mi codigo de trabajador '{codigo}'")
+def step_impl(context,hora,minuto,segundo,dia,codigo):
+	mensaje,turno = marcar_hora_entrada(codigo, dia, int(hora),int(minuto), int(segundo))
+	context.mensaje=mensaje
+	context.turno=turno
+
+@when("se ingrese mi hora de entrada '{hora}' hora '{minuto}' minutos '{segundo}' el dia '{dia}' con un retraso mayor o igual a 15 minutos, pero menor a 1 hora, e ingrese mi codigo de trabajador '{codigo}'")
+def step_impl(context,hora,minuto,segundo,dia,codigo):
+	mensaje,turno = marcar_hora_entrada(codigo, dia, int(hora),int(minuto), int(segundo))
+	context.mensaje=mensaje
+	context.turno=turno
+
+@when("se ingrese mi hora de entrada '{hora}' hora '{minuto}' minutos '{segundo}' el dia '{dia}' antes de lo esperado (20 minutos antes de mi hora de entrada) e ingrese mi codigo de trabajador '{codigo}'")
+def step_impl(context,hora,minuto,segundo,dia,codigo):
+	mensaje,turno = marcar_hora_entrada(codigo, dia, int(hora),int(minuto), int(segundo))
+	context.mensaje=mensaje
+	context.turno=turno
