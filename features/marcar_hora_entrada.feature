4554744081fe5b4a1c09fa46469e2aa882dc27ef
Miguel Angel Sanchez Romero <mangelsr@live.com>
2019-08-26 04:46:03 -0500

    [ADD] Pruebas de aceptacion

--- features/marcar_hora_entrada.feature
+++ features/marcar_hora_entrada.feature
@@ -2,9 +2,39 @@
 
 Característica: Registrar hora de entrada
 
- @jornadasDeIngreso
-  Escenario: 
-      Dado que se cumplen los requisitos
-      Cuando se ejecute una accion
-      Entonces genera el siguiete resultado 'resultado_variable'
-      Y tambien ocurre lo siguiente (si es necesario)
+    @jornadasDeIngreso
+    Escenario: 
+        Dado que se cumplen los requisitos
+        Cuando se ejecute una accion
+        Entonces genera el siguiete resultado 'resultado_variable'
+        Y tambien ocurre lo siguiente (si es necesario)
+
+
+    @jornadasDeIngreso
+    Escenario: El trabajador incia su jornada normalmente
+        Dado que mi horario de trabajo puede ser diurno o nocturno
+        Cuando ingrese mi hora de entrada 'Lunes 7:00:00'
+        Y ingrese mi codigo de trabajador '20131'
+        Entonces se buscara mi nombre en la lista de los trabajadores
+        Y aparecera el siguiente mensaje 'Inicio de jornada a tiempo, Bienvenido Luis Zuniga.'
+        Y devolvera el tipo de jornada 'Diurno A'
+
+
+    @jornadasDeIngreso
+    Escenario: El trabajador incia su jornada tarde
+        Dado que mi horario de trabajo puede ser diurno o nocturno
+        Cuando ingrese mi hora de entrada 'Lunes 7:20:00'
+        Y ingrese mi codigo de trabajador '20131'
+        Entonces se buscara mi nombre en la lista de los trabajadores
+        Y aparecera el siguiente mensaje 'Inicio de Jornada atrasada por 00:20:00.'
+        Y devolvera el tipo de jornada 'Diurno A'
+
+
+    @jornadasDeIngreso
+    Escenario: El trabajador incia su jornada tarde
+        Dado que mi horario de trabajo puede ser diurno o nocturno
+        Cuando ingrese mi hora de entrada 'Lunes 7:20:00'
+        Y ingrese mi codigo de trabajador '20131'
+        Entonces se buscara mi nombre en la lista de los trabajadores
+        Y aparecera el siguiente mensaje 'Aun no empieza su turno de trabajo'
+        Y devolvera el tipo de jornada 'Diurno A'
--- features/steps/jornadas_laborales.py
+++ features/steps/jornadas_laborales.py
@@ -2,24 +2,66 @@ from behave import *
 from src.Jornadas import *
 
 
-#Condiciones antes de empezar cualquier STEP
+# Condiciones antes de empezar cualquier STEP
 def before_scenario(context, scenario):
-	context = {}
+    context = {}
 
 
 @given("que se cumplen los requisitos")
 def step_impl(context):
-	pass
+    pass
+
 
 @when("se ejecute una accion")
 def step_impl(context):
-	pass
+    pass
+
 
 @then("genera el siguiete resultado '{variable}'")
 def step_impl(context, variable):
-	print(variable)
-	pass
+    print(variable)
+    pass
+
 
 @then("tambien ocurre lo siguiente (si es necesario)")
 def step_impl(context):
-	pass
+    pass
+
+
+@given("que mi horario de trabajo puede ser diurno o nocturno")
+def step_impl(context):
+    pass
+
+
+@when("ingrese mi hora de entrada '{hora_entrada}'")
+def step_impl(context, hora_entrada):
+    hora_entrada = hora_entrada.split(" ")
+    dia = hora_entrada[0]
+    tiempo = hora_entrada[1].split(":")
+    context['dia'] = dia
+    context['hora'] = int(tiempo[0])
+    context['minutos'] = int(tiempo[1])
+    context['segundos'] = int(tiempo[0])
+
+
+@when("ingrese mi codigo de trabajador '{cod_trabajador}'")
+def step_impl(context, cod_trabajador):
+    context['cod_trabajador'] = cod_trabajador
+
+
+@then("se buscara mi nombre en la lista de los trabajadores")
+def step_impl(context, variable):
+    mensaje, empleado = marcar_hora_entrada(context['cod_trabajador'],
+                                            context['dia'], context['hora'],
+                                            context['minutos'],
+                                            context['segundos'])
+    context['mensaje'] = mensaje
+    context['empleado'] = empleado
+
+@then("aparecera el siguiente mensaje '{msg}'")
+def step_impl(context, msg):
+	assert(context['mensaje'], msg)
+
+@then("devolvera el tipo de jornada '{jornada}'")
+def step_impl(context, jornada):
+	assert(context['empleado'](1), jornada)
