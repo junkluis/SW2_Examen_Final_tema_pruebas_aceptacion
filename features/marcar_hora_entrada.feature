e904880ff5422020a15609cf7f35f41d3dc2cd7f
Steeven Rodriguez <rodriguez_steven11@hotmail.com>
2019-08-26 09:47:39 -0500

    Inicio de prueba 2

--- features/marcar_hora_entrada.feature
+++ features/marcar_hora_entrada.feature
@@ -8,3 +8,11 @@ Característica: Registrar hora de entrada
       Cuando se ejecute una accion
       Entonces genera el siguiete resultado 'resultado_variable'
       Y tambien ocurre lo siguiente (si es necesario)
+
+ @jornadaMarcarIngreso
+  Escenario:
+    Dado que mi horario sea 'martes'
+    Cuando ingrese mi hora de entrada en hora '13', minutos '30', segundos '0' y mi codigo '20193'
+    Entonces se buscara mi nombre en la lista de empleados
+    Y aparecera el mensaje 'Inicio de Jornada a tiempo, Bienvenido Arya Stark'
+    Y devolvera mi jornada laboral 'Diurno B'
--- features/steps/jornadas_laborales.py
+++ features/steps/jornadas_laborales.py
@@ -6,6 +6,35 @@ from src.Jornadas import *
 def before_scenario(context, scenario):
 	context = {}
 
+@given("que mi horario sea: '{jornada}'")
+def step_impl(context, jornada):
+    context.dia = jornada
+
+@when("ingrese mi hora de entrada en hora: '{hora}', minutos: '{minutos}', segundos: '{segundos}' y mi codigo: '{codigo}'")
+def step_impl(context, hora, minutos, segundos, codigo):
+    context.hora = int(hora)
+    context.minutos = int(minutos)
+    context.segundos = int(segundos)
+    context.codigo = codigo
+
+    mensaje, jornada = marcar_hora_entrada(context.codigo,context.dia,context.hora,context.minutos, context.segundos)
+    print(resultado)
+    context.mensaje = mensaje
+    context.jornada = jornada
+
+@then("se buscara mi nombre en la lista de empleados")
+def step_impl(context):
+    pass
+@then("aparecera el mensaje: '{mensaje}'")
+def step_impl(context, mensaje):
+    print(mensaje)
+    print(context.mensaje)
+    assert context.mensaje == mensaje
+@then("devolvera mi jornada laboral: '{jornada}'")
+def step_impl(context, jornada):
+    print(jornada)
+    print(context.jornada)
+    assert context.jornada == jornada
 
 @given("que se cumplen los requisitos")
 def step_impl(context):
