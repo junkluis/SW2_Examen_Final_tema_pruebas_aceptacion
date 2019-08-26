9a50b898540480ccea91d1d97590a3a51ac1034c
rrobayo <rrobayo@espol.du.ec>
2019-08-26 10:23:39 -0500

    pruebas

--- features/steps/jornadas_laborales.py
+++ features/steps/jornadas_laborales.py
@@ -6,3 +6,20 @@ from src.Jornadas import *
 def before_scenario(context, scenario):
 	context = {}
 
+
+@given("que mi horario de trabajo puede ser '{horario}'")
+def step_impl(context, horario):
+	assert horario in ['diurno','nocturno']
+
+@when("ingrese mi horario de entrada: '{entrada}' el dia: '{dia}' , código: '{codigo}'")
+def step_impl(context, entrada, dia, codigo):
+	hora, minutos, segundos = entrada.split(':')
+	mensaje, jornada = marcar_hora_entrada(codigo, dia, int(hora), int(minutos), int(segundos))
+	context.mensaje = mensaje
+	context.jornada = jornada
+
+@then("aparecerá el mensaje: '{mensaje}' y mi jornada laboral '{jornada}'")
+def step_impl(context, mensaje, jornada):
+	context.mensaje = context.mensaje[:len(mensaje)]
+	assert context.mensaje == mensaje
+	assert context.jornada == jornada
